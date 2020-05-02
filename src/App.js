068348e77ef5be02a0a6289c5380b82d96f68748
Juan Petrus <juan.petruss@gmail.com>
2020-05-02 15:29:13 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.scss";
+import { Container, Section } from 'rbx';
+import Header from "./components/header";
+import Routes from './routes';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <div>
+    <Header />
+    <Section>
+      <Container>
+        <Routes />
+      </Container>
+    </Section>
+  </div>
+)
 
 export default App;
---
+++ src/screens/restaurants/index.js
@@ -0,0 +1,10 @@
+import React, { Fragment } from 'react';
+import ListRestaurants from "../../components/list_restaurants";
+
+const Restaurants = () => (
+  <Fragment>
+    <ListRestaurants />
+  </Fragment>
+);
+
+export default Restaurants;
